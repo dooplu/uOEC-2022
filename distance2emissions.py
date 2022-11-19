3a692fe7caf3387effbc1498591dfd8d0301f917
Adam Dia <43157907+dooplu@users.noreply.github.com>
2022-11-19 13:26:55 -0500

    Update distance2emissions.py

--- distance2emissions.py
+++ distance2emissions.py
@@ -1,7 +1,18 @@
-def co2Emitted(distance, mass):
+from geopy.geocoders import Nominatim
+from geopy import distance as dist
+
+def co2Emitted(home, shipper, mass):
     maxLandDistanceinKM = 3500
     co2pertonnekmships = 4
     co2pertonnekmtrucks = 63.4
+
+    geolocator = Nominatim('default_user_agent')
+    origin = geolocator.geocode(shipper)
+    origin = (origin.longitude,origin.latitude)
+    destination = geolocator.geocode(home)
+    destination = (destination.longitude, destination.latitude)
+
+    distance = dist.distance(origin, destination).kilometers
     
     if (distance > maxLandDistanceinKM):
         shipDistance = distance - maxLandDistanceinKM
